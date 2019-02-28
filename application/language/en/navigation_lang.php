43b624cd138d83e020109dff6f140292d84ffdfe
Ali Metwally <unique.business.sw@gmail.com>
2019-02-28 09:17:01 +0200

    translate template
    add navigation translation files (ar-en)
    
    add arabic and english links to template
    translate template

--- application/language/en/navigation_lang.php
+++ application/language/en/navigation_lang.php
@@ -1,7 +1,28 @@
 <?php
 
-$lang['nav_title'] = 'abuqir Fert';
+# website title
+$lang['abuqir'] = 'abuqir';
+$lang['fertilizers'] = 'fertilizers';
 
-$lang['home_link'] = 'Home';
+# navigation
+$lang['home_link'] = 'home';
+$lang['about_link'] = 'about us';
+$lang['media_link'] = 'media';
+$lang['reponsibility_link'] = 'Social Resposibility';
+$lang['investors_link'] = 'investors relations';
+$lang['contact_link'] = 'contact';
+
+# footer headers 
+$lang['contact'] = 'Stay Connecting';
+$lang['visit'] = 'Visit';
+$lang['about'] = 'About Us';
+
+# contact data
+$lang['address'] = 'Address: EL-Tabia- Rashid Road';
+$lang['state'] = 'Alexandria, Egypt';
+$lang['mobile'] = 'Mobile : (201) 001-625632';
+$lang['phone'] = 'Phone : (203) 560-3053';
 
 
+# about us footer statment 
+$lang['about_statement'] = 'We work efficiently, safety and environmentally responsible manner to produce our products at the quality required by our customer and carryout investments to maximize returns.';
