0c58cbebe60623dbf31498b39b7daaddf61c6372
Ali Metwally <unique.business.sw@gmail.com>
2019-02-28 13:42:27 +0200

    translate homepage

--- application/hooks/LanguageLoader.php
+++ application/hooks/LanguageLoader.php
@@ -6,11 +6,18 @@ class LanguageLoader {
         $ci = & get_instance();
         
         $site_lang = $ci->session->userdata('site_lang');
-
-        if ($site_lang) {
-            $ci->lang->load('navigation', $ci->session->userdata('site_lang'));
-        } else {
-            $ci->lang->load('navigation', 'ar'); // default language = arabic
+        
+        $controller = $ci->router->fetch_class();
+        $method = $ci->router->fetch_method();
+        
+        if (! $site_lang)  {$ci->session->set_userdata('site_lang','ar');}// set language to arabic
+        
+        // load naviagation translation for template 
+        $ci->lang->load('navigation', $ci->session->userdata('site_lang'));
+        
+        
+        if($controller == 'home' && $method == 'index' ){
+            $ci->lang->load('home', $ci->session->userdata('site_lang'));
         }
     }
 
