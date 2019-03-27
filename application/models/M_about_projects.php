a79c9536bc86b4b8d4e975be91fc4d882342500a
karim <sbycrims@gmail.com>
2019-03-27 19:04:46 +0200

    update new projects

---
+++ application/models/M_about_projects.php
@@ -0,0 +1,49 @@
+<?php
+
+class M_about_projects extends CI_Model {
+    function __construct() {
+        parent::__construct();
+        $this->lang = $this->session->site_lang;
+        
+    }
+
+    public function get_projects(){
+        $table = 'projects a,projects_contents b';
+        $project_name = $this->lang == 'ar'? 'a.project_name_ar as project_name': 'a.project_name_en as project_name';
+        $content = $this->lang == 'ar'? 'SUBSTRING_INDEX(TRIM(b.content_ar), \' \', 9) as content': 'SUBSTRING_INDEX(TRIM(b.content_en), \' \', 9) as content';
+        $columns = "a.project_id,$project_name, b.head_id,b.content_id,$content";
+        $where = ' b.head_id = 1 and b.content_id = 1 and a.project_id = b.project_id';
+        $this->db->order_by('a.project_id');
+        $query = $this->model_db->read($table, $columns, $where);        
+        return $query;
+    
+    }
+
+    public function get_projects_name($project){
+        $table = 'projects a';
+        $columns = "a.project_id,a.project_name_ar,a.project_name_en,a.images";
+        $where = ' a.project_id = '.$project;
+        // $this->db->order_by('a.project_id');
+        $query = $this->model_db->getSingleRow($table, $columns, $where);        
+        return $query;
+    }
+
+    public function get_projects_header($project){
+        $table = 'projects_headers b';
+        $columns = "b.project_id,b.head_id,b.head_name_ar,b.head_name_en";
+        $where = ' b.project_id = '.$project;
+        $query = $this->model_db->read($table, $columns, $where);        
+        return $query;
+    }
+
+    public function get_projects_content($project,$header){
+        $table = 'projects_contents c';
+        $columns = "c.content_id,c.content_ar,c.content_en";
+        $where = " c.head_id = ".$header." and c.project_id = ".$project;
+        // $this->db->order_by('a.project_id');
+        $query = $this->model_db->read($table, $columns, $where);        
+        return $query;
+        
+    }
+
+}
--- application/views/pages/about/v_projects.php
+++ application/views/pages/about/v_projects.php
@@ -7,8 +7,8 @@
             <div class="col m-4  text-center">
                 <div class="sec-title mb-20">
                     <h2 class="text-muted">
-                    <?= lang('contact_title')?> <span class="title-span text-main-color">
-                        <?= lang('title_span')?> </span>
+                        <?= lang('project_title')?> <span class="title-span text-main-color">
+                            <?= lang('project_span')?> </span>
                         <span class="sec-title-border">
                             <span></span>
                             <span></span>
@@ -19,7 +19,33 @@
             </div>
         </div>
         <!-- end of title -->
-        
-        
+        <div class="row">
+            <div class="col-lg-6">
+                <img class="w-100" src="<?= base_url('/public/images/projects_intro.png')?>" alt="">
+            </div>
+            <div class="col-lg-6 mt-3 mt-md-0">
+                <p class="lead"><?= lang('project_p')?></p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="bg-white py-5 new-projects">
+    <div class="container">
+        <div class="row justify-content-around">
+            <?php foreach ($projects as $pro):  ?>
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="project-box">
+                        <div class="project-icon">
+                            <i class="fa fa-industry" aria-hidden="true"></i>
+                        </div>
+                        <div class="project-text">
+                            <h5><?= $pro->project_name?></h5>
+                            <p class="small"><?= $pro->content?> ....</p>
+                            <a href="<?= base_url('about/project_details/').$pro->project_id?>" class="btn btn-outline-main">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach;?>
+        </div>
     </div>
 </section>
