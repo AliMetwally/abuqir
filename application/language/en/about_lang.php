d30b94e60111b54a5de85cb06581fe097852dfdb
Ali Metwally <unique.business.sw@gmail.com>
2019-03-31 13:05:29 +0200

    Certificates Page

--- application/language/en/about_lang.php
+++ application/language/en/about_lang.php
@@ -19,17 +19,17 @@ $lang['p1']= "On 20 July 1976 Abu Qir Fertilizers Company was founded by the min
 $lang['p2']= "On 20 April 1980 the company was declared as an Egyptian shareholding company according to the ministerial decree of Minister of economy and foreign trade and international cooperation number 107 of the year 1980 and according to the law 43 of the year 1974";
 $lang['p3']= "The company is regulated by the law 97 of the year 1983";
 $lang['p4']= "On 15 May 1996 the ministerial committee of the public sector companies and the privatization program had agreed that the company will be regulated by the law 159 of the year 1981 an this was approved by the general assembly of the company on 16 May 1996";
-$lang['p5']= "The capital of the company is 100% Egyptian";
-$lang['p6']= "The company is a pioneer of producing and marketing nitrogen fertilizers and liquid and mixed fertilizers in Egypt and Africa in terms of production rates and the diversity of the products.";
-$lang['p7']= "In 1979 Abu Qir 1 Factory was inaugurated to produce regular Urea Fertilizers with a production capacity of 1550 tons/day then increase efficiency of plant to achieved 1750 ton/day.";
-$lang['p8']= "In the year 1991 Abu Qir 2 Factory was inaugurated to produce ammonium nitrate Fertilizers with a production capacity of 2400 tons/day";
-$lang['p9']= "In 1998 Abu Qir 3 Factory was inaugurated to produce granulated Urea Fertilizers with a production capacity of 1750 tons/day then increase efficiency of plant to achieved 1925 ton/day.";
-$lang['p10']= "In 2004 Urea magnesium fertilizer was produced in Abu Qir 3 Factory";
-$lang['p11']= "In 2004 Urea sulfate fertilizer was produced in Abu Qir 3 Factory";
-$lang['p12']= "In 2005 Urea zinc fertilizer was produced in Abu Qir 1 Factory";
-$lang['p13']= "In 2005 the mixed fertilizers factory was inaugurated to produce mixed fertilizers with a production capacity of 1000 tons/day";
-$lang['p14']= "In 2006 the liquid fertilizers factory was inaugurated to produce urea ammonium nitrate solution (UAN) with a production capacity of 1000 tons/day then increase efficiency of plant to achieved 2400 ton/day.";
-$lang['p15']= "In 2012 the production capacity of the liquid fertilizers factory was increased to be 2000 tons/day";
+$lang['p5'] = "In 1996 increase efficiency of ammonia plant in Abu Qir 1 to increase ammonia production from 1000 ton/day to 1100 ton/day then to 1150 ton /day in 2012.";
+$lang['p6']= "The capital of the company is 100% Egyptian";
+$lang['p7']= "The company is a pioneer of producing and marketing nitrogen fertilizers and liquid and mixed fertilizers in Egypt and Africa in terms of production rates and the diversity of the products.";
+$lang['p8']= "In 1979 Abu Qir 1 Factory was inaugurated to produce regular Urea Fertilizers with a production capacity of 1550 tons/day then increase efficiency of plant to achieved 1750 ton/day.";
+$lang['p9']= "In the year 1991 Abu Qir 2 Factory was inaugurated to produce ammonium nitrate Fertilizers with a production capacity of 2400 tons/day";
+$lang['p10']= "In 1998 Abu Qir 3 Factory was inaugurated to produce granulated Urea Fertilizers with a production capacity of 1750 tons/day then increase efficiency of plant to achieved 1925 ton/day.";
+$lang['p11']= "In 2004 Urea magnesium fertilizer was produced in Abu Qir 3 Factory";
+$lang['p12']= "In 2004 Urea sulfate fertilizer was produced in Abu Qir 3 Factory";
+$lang['p13']= "In 2005 Urea zinc fertilizer was produced in Abu Qir 1 Factory";
+$lang['p14']= "In 2005 the mixed fertilizers factory was inaugurated to produce mixed fertilizers with a production capacity of 1000 tons/day";
+$lang['p15']= "In 2006 the liquid fertilizers factory was inaugurated to produce urea ammonium nitrate solution (UAN) with a production capacity of 1000 tons/day then increase efficiency of plant to achieved 2400 ton/day in 2012.";
 $lang['p16']= "In 2014 Eurofert and Nitrofert Fertilizers was produced";
 $lang['p17']= "In 2018 increase efficiency of ammonia plant in Abu Qir 3 to increase ammonia production from 1200 ton/day to 1230 ton/day.";
 $lang['p18']= "In 2019 Plastic Factory was inaugurated to produce fertilizers Plastic Bags with a production capacity of 40 mn bag/year.";
@@ -91,3 +91,55 @@ $lang['UAN_head'] = "UAN UNIT";
 $lang['UAN_title1'] = "Products";
 $lang['UAN_p1_1'] = " Urea Ammonium Nitrate Solution 32% N2.";
 $lang['UAN_p1_2'] = " Production Start date August 2006";
+
+#-------------------------------------------------------------------------------
+
+# Certificates Page
+$lang['certificate_title'] = 'Certificates';
+$lang['certificate_download'] = 'Download';
+
+# ISO14001
+$lang['ISO14001_name'] = 'ISO 14001';
+$lang['ISO14001_1'] = ' ISO 14001:2015 provides practical tools for companies and organizations of all kinds looking to manage their environmental responsibilities.';
+$lang['ISO14001_2'] = ' Certificate scope for Production and marketing of Ammonia, Urea, Ammonium Nitrate, Bulk Blended Fertilizers and Urea Ammonium Nitrate Liquid Fertilizers.';
+$lang['ISO14001_3'] = '';
+
+
+# ISO 9001
+$lang['ISO9001_name'] = 'ISO 9001';
+$lang['ISO9001_1'] = ' ISO 9001:2015 - sets out the requirements of a quality management system.';
+$lang['ISO9001_2'] = ' Certificate scope for Production and marketing of Ammonia, Urea, Ammonium Nitrate, Bulk Blended Fertilizers and Urea Ammonium Nitrate Liquid Fertilizers.';
+$lang['ISO9001_3'] = '';
+
+# OHSAS 18001
+$lang['18001_name'] = 'OHSAS 18001';
+$lang['18001_1'] = ' OHSAS 18001:2007 - Occupational health and safety issues are the most important priorities at Abu Qir.';
+$lang['18001_2'] = ' Company implements a comprehensive occupational health and safety management system that conforms to the latest international standards. AFC safety system comprises an incident prevention program, including emergency preparedness plans, training, safety procedures, protective equipment.';
+$lang['18001_3'] = ' The company has been certified against Occupational Health and Safety Assessment Service (OHSAS) 18001:2007 standard in 2009.';
+$lang['18001_4'] = ' To ensure the continual improvement in our safety performance, we are regularly audited by external auditors to review and verify our compliance with the standard.';
+
+# RECHA
+$lang['RECHA_name'] = 'RECHA';
+$lang['RECHA_1'] = ' Abu Qir products are registered for (REACH) the new European chemicals legislation.';
+
+# EGAC
+$lang['EGAC_name'] = 'EGAC';
+$lang['EGAC_1'] = ' The Egyptian Accreditation Council is recognized by the Presidential Decree number 312/1996 as the sole national body for the assessment and accreditation of conformity assessment bodies performing testing/ calibration Laboratories, inspection and certification of products & systems as well as personnel . EGAC is headed by the Minister of Trade & Industry and governed by a board of 14 members, representing all stakeholders and concerned bodies.';
+
+# Energy
+$lang['Energy_name'] = 'Energy management system';
+$lang['Energy_1'] = ' Abu Qir Fertilizer Company has been assessed and certified as meeting the requirements of ISO 50001: 2011.';
+$lang['Energy_2'] = ' Energy management system of Abu Qir Fertilizers Company is reviewed annually by external auditors from SGS to ensure that our system meets the requirements of the standard.';
+$lang['Energy_3'] = " Energy management system implementation aims to ensure the continual improvement in our energy performance and use it with the highest possible efficiency and minimum consumption to conserve natural resources, which supporting the company's vision of sustainability principles.";
+
+# Supply Chain
+$lang['Supply_name'] = 'Supply Chain Security Management system';
+$lang['Supply_1'] = ' Abu Qir Fertilizer Company has been assessed and certified as meeting the requirements of ISO 28000: 2007.';
+$lang['Supply_2'] = ' Supply chain security management system of Abu Qir Fertilizers Company is reviewed annually by external auditors from SGS to ensure that our system meets the requirements of the standard.';
+$lang['Supply_3'] = ' Supply chain security management system of the company aims to secure the sources of raw materials for the company and product for farmers passed through the processes to ensure the continuity of business.';
+
+# IFA
+$lang['IFA_name'] = 'IFA protect and sustain product stewardship excellence.';
+$lang['IFA_1'] = ' Abu Qir fertilizers has achieved this certificate in March 2017.';
+$lang['IFA_2'] = ' IFA 12 HSE principles is reviewed annually by external auditors from SGS.';
+$lang['IFA_3'] = ' IFA P&S principles aims is maintaining business continuity, protecting employees, optimizing the use of resources, preserving the environment, care of farmers needs and preventing the misuse of products.';
--- application/views/pages/about/v_certificates.php
+++ application/views/pages/about/v_certificates.php
@@ -7,8 +7,8 @@
             <div class="col m-4  text-center">
                 <div class="sec-title mb-20">
                     <h2 class="text-muted">
-                    <?= lang('contact_title')?> <span class="title-span text-main-color">
-                        <?= lang('title_span')?> </span>
+                        <!-- <?= lang('contact_title') ?>  --> 
+                        <span class="title-span text-main-color"><?= lang('certificate_title') ?> </span>
                         <span class="sec-title-border">
                             <span></span>
                             <span></span>
@@ -19,7 +19,166 @@
             </div>
         </div>
         <!-- end of title -->
+
+        <!--ISO14001-->
+        <div class="card mb-4">
+            <div class="row no-gutters cert-card">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img style="height: 300px" src="<?= base_url('public/images/certificates/14001.jpg') ?>" class="card-img" alt="...">
+                        <a href="<?= base_url('public/images/certificates/14001.jpg') ?>" class="btn btn-main"><?= lang('certificate_download') ?> <i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('ISO14001_name') ?></h4>
+                        <p class="card-text"><?= lang('ISO14001_1') ?></p>
+                        <p class="card-text"><?= lang('ISO14001_2') ?></p>
+                        <p class="card-text"><?= lang('ISO14001_3') ?></p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--ISO 9001-->
+        <div class="card mb-4">
+            <div class="row no-gutters cert-card">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img style="height: 300px" src="<?= base_url('public/images/certificates/9001.jpg') ?>" class="card-img" alt="...">
+                        <a href="<?= base_url('public/images/certificates/9001.jpg') ?>" class="btn btn-main"><?= lang('certificate_download') ?> <i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('ISO9001_name') ?></h4>
+                        <p class="card-text"><?= lang('ISO9001_1') ?></p>
+                        <p class="card-text"><?= lang('ISO9001_2') ?></p>
+                        <p class="card-text"><?= lang('ISO9001_3') ?></p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!--OHSAS 18001-->
+        <div class="card mb-4">
+            <div class="row no-gutters cert-card">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img style="height: 350px" src="<?= base_url('public/images/certificates/safty.jpg') ?>" class="card-img" alt="...">
+                        <a href="<?= base_url('public/images/certificates/safty.jpg') ?>" class="btn btn-main"><?= lang('certificate_download') ?> <i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('18001_name') ?></h4>
+                        <p class="card-text"><?= lang('18001_1') ?></p>
+                        <p class="card-text"><?= lang('18001_2') ?></p>
+                        <p class="card-text"><?= lang('18001_3') ?></p>                        
+                        <p class="card-text"><?= lang('18001_4') ?></p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!--RECHA-->
+        <div class="card mb-4">
+            <div class="row no-gutters cert-card">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img style="height: 150px" src="<?= base_url('public/images/certificates/European_chemicals_agency_logo.jpg') ?>" class="card-img" alt="...">
+                        <a href="#" onclick='window.open("<?= base_url('uploads/REACH_certificates/REACH AMMONIA.pdf')?>"); ' class="btn btn-main"><?= lang('certificate_download') ?> <i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('RECHA_name') ?></h4>
+                        <p class="card-text"><?= lang('RECHA_1') ?></p>
+                      
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!--EGAC-->
+        <div class="card mb-4">
+            <div class="row no-gutters cert-card">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img style="height: 350px" src="<?= base_url('public/images/certificates/egac_en.jpg') ?>" class="card-img" alt="...">
+                        <a href="<?= base_url('public/images/certificates/egac_en.jpg') ?>" class="btn btn-main"><?= lang('certificate_download') ?> <i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('EGAC_name') ?></h4>
+                        <p class="card-text"><?= lang('EGAC_1') ?></p>
+                        <p class="card-text"><?= lang('EGAC_2') ?></p>
+                        <p class="card-text"><?= lang('EGAC_3') ?></p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!--Energy management system-->
+        <div class="card mb-4">
+            <div class="row no-gutters cert-card">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img style="height: 350px" src="<?= base_url('public/images/certificates/50001.jpg') ?>" class="card-img" alt="...">
+                        <a href="<?= base_url('public/images/certificates/50001.jpg') ?>" class="btn btn-main"><?= lang('certificate_download') ?> <i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('Energy_name') ?></h4>
+                        <p class="card-text"><?= lang('Energy_1') ?></p>
+                        <p class="card-text"><?= lang('Energy_2') ?></p>
+                        <p class="card-text"><?= lang('Energy_3') ?></p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!--Supply Chain Security Management system.-->
+        <div class="card mb-4">
+            <div class="row no-gutters cert-card">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img style="height: 350px" src="<?= base_url('public/images/certificates/28000.jpg') ?>" class="card-img" alt="...">
+                        <a href="<?= base_url('public/images/certificates/28000.jpg') ?>" class="btn btn-main"><?= lang('certificate_download') ?> <i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('Supply_name') ?></h4>
+                        <p class="card-text"><?= lang('Supply_1') ?></p>
+                        <p class="card-text"><?= lang('Supply_2') ?></p>
+                        <p class="card-text"><?= lang('Supply_3') ?></p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
         
         
-    </div>
+        <!--IFA-->
+        <div class="card mb-4">
+            <div class="row no-gutters cert-card">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img style="height: 350px" src="<?= base_url('public/images/certificates/ifa.jpg') ?>" class="card-img" alt="...">
+                        <a href="<?= base_url('public/images/certificates/ifa.jpg') ?>" class="btn btn-main"><?= lang('certificate_download') ?> <i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('IFA_name') ?></h4>
+                        <p class="card-text"><?= lang('IFA_1') ?></p>
+                        <p class="card-text"><?= lang('IFA_2') ?></p>
+                        <p class="card-text"><?= lang('IFA_3') ?></p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- end container -->
 </section>
