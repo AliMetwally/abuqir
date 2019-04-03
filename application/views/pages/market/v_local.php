69863958b67c5f90bf46ebbc78e968645f7b5e6b
Ali Metwally <unique.business.sw@gmail.com>
2019-04-03 10:37:55 +0200

    css compile

--- application/views/pages/market/v_local.php
+++ application/views/pages/market/v_local.php
@@ -19,4 +19,31 @@
             </div>
         </div>
         <!-- end of title -->
+        
+        <div class="row bg-white">
+            <div class="col-12 p-0">
+                <div class="card-body p-0">
+                    <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('granular_urea_head')?></h4>
+                    <p class="px-4">text</p>
+                    
+                </div>
+                
+                <div class="card-body p-0">
+                    <h4 class="card-title bg-main py-3 px-2 text-white"><?= lang('granular_urea_head')?></h4>
+                    <p class="px-4">text</p>
+                    <p class="px-4">text</p>
+                    <p class="px-4">text</p>
+                    <h5 class="px-3"><?= lang('overview_head') ?></h5> <!-- plant name -->
+                    <p class="px-4">text</p>
+                    
+                    <h5 class="px-3"><?= lang('overview_head') ?></h5> <!-- plant name -->
+                    <p class="px-4">text</p>
+                    
+                    <h5 class="px-3"><?= lang('overview_head') ?></h5> <!-- plant name -->
+                    <p class="px-4">text</p>
+                </div>
+            </div>
+        </div>
+        
+    </div> <!-- end container -->
 </section>
